e07f2a055bbfe75e6a4d147431e04d61cb18a2f2
Sarvesh <sarveshnirmalkar@gmail.com>
2025-09-28 17:34:55 +0530

    updated user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema(
   {
@@ -60,5 +61,15 @@ const userSchema = new Schema(
     timestamps: true,
   },
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model('User', userSchema);
